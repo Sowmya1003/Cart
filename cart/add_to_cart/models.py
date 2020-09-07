6d282065941880f85263cc5f67333494f9311d67
sowmya <sowmyasrigundameedi@gmail.com>
2020-09-07 21:14:46 +0530

    did minor changes

--- cart/add_to_cart/models.py
+++ cart/add_to_cart/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Item:
+    id: str
     name: str
     desc: str
     price: str
--- cart/add_to_cart/views.py
+++ cart/add_to_cart/views.py
@@ -1,38 +1,48 @@
 from django.shortcuts import render
 from .models import Item
 
+def post(request):
+    Item = request.POST.get(Item)
+    print(Item)
+    # return render(request,'home.html')
+
 def product_list(request):
     item1 = Item()
     item1.name = "Car"
     item1.desc = "On Sale"
     item1.price = "Price: $500"
+    item1.id = "1"
 
     item2 = Item()
     item2.name = "Phone"
     item2.desc = "New Item"
     item2.price = "Price: $40"
+    item2.id = "2"
 
     item3 = Item()
     item3.name = "Car"
     item3.desc = " For Very Smooth Driving"
     item3.price = "Price: $500"
+    item3.id = "3"
 
     item4 = Item()
     item4.name = "Car"
     item4.desc = "On Sale"
     item4.price = "Price: $40"
-
+    item4.id = "4"
 
     item5 = Item()
     item5.name = "Phone"
     item5.desc = "New Item"
     item5.price = "Price: $500"
+    item5.id = "5"
 
-    
     item6 = Item()
     item6.name = "Car"
     item6.desc = " For Very Smooth Driving"
     item6.price = "Price: $40"
+    item6.id = "6"
+
 
     
     items = [item1,item2,item3,item4,item5,item6]
